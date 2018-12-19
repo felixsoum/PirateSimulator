45ac1d99df0fa6f7afb05035388ba65dd2f3eae4
Unknown <felix@felixsoum.com>
2018-12-19 03:46:34 -0500

    Add initial project

---
+++ Assets/Scripts/PlayerShip.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShip : MonoBehaviour
+{
+    [SerializeField] float speed = 10.0f;
+    [SerializeField] float maxSpeed = 10.0f;
+
+    new Rigidbody2D rigidbody2D;
+
+    void Awake()
+    {
+        rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void FixedUpdate()
+    {
+        rigidbody2D.AddForce(transform.up * speed * Time.fixedDeltaTime, ForceMode2D.Impulse);
+        Vector2 velocity = rigidbody2D.velocity;
+        if (velocity.magnitude > maxSpeed)
+        {
+            rigidbody2D.velocity = velocity.normalized * maxSpeed;
+        }
+    }
+}
